5a8c8e0f8c7e85e2dce791fdc91a14f9fe54fd28
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2016-11-30 20:49:37 -0600

    Prueba de un nuevo producto

--- js/index.js
+++ js/index.js
@@ -526,47 +526,20 @@ var app = {
 		
 		
 		
-		inAppPurchase.getProducts(['com.revistamoto.revista03', 'revista03', 'com.revistamoto.revista02', 'rev002', '1177281434', '1179868852']).then(function (products) {
+		inAppPurchase.getProducts(['com.revistamoto.app.revista04']).then(function (products) {
 			console.log(products);
 		}).catch(function (err) {
 			console.log(err);
 		});
 		
-		inAppPurchase.buy('com.revistamoto.revista03').then(function (data) {
+		inAppPurchase.buy('com.revistamoto.app.revista04').then(function (data) {
 			console.log(data);
 		}).catch(function (err) {
 			console.log(err);
 		});
 		
-		inAppPurchase.buy('revista03').then(function (data) {
-			console.log(data);
-		}).catch(function (err) {
-			console.log(err);
-		});
-		
-		inAppPurchase.buy('com.revistamoto.revista02').then(function (data) {
-			console.log(data);
-		}).catch(function (err) {
-			console.log(err);
-		});
-		
-		inAppPurchase.buy('rev002').then(function (data) {
-			console.log(data);
-		}).catch(function (err) {
-			console.log(err);
-		});
-		
-		inAppPurchase.buy('1177281434').then(function (data) {
-			console.log(data);
-		}).catch(function (err) {
-			console.log(err);
-		});
-		
-		inAppPurchase.buy('1179868852').then(function (data) {
-			console.log(data);
-		}).catch(function (err) {
-			console.log(err);
-		});
+
+
 	}
 };
 
