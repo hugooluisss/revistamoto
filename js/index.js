616b6723f5351450c608f887d5d2dfd1830cd33d
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2016-12-27 09:02:04 -0600

    Se sube un nuevo identificador

--- js/index.js
+++ js/index.js
@@ -354,17 +354,9 @@ var app = {
 			debug:    true, // Enable IAP messages on the console
 			ready:    function(){ 
 				 /*puede ser un array de strings ['pro1',['prod2'],...*/
-				storekit.load(['com.revistamoto.appios.edicion159', 'edicion159', 'edicion160'], function (products, invalidIds) {
+				storekit.load(['com.revistamoto.appios.edicion159', 'edicion159', 'edicion160', '1190032144'], function (products, invalidIds) {
 					//se deben cargar los productos de la tienda para poder usarlos después			     
 					console.log(products, invalidIds);
-					
-					$.get("https://buy.itunes.apple.com/verifyReceipt/", function(resp){
-						console.info(resp);
-					});
-					
-					$.get("https://sandbox.itunes.apple.com/verifyReceipt/", function(resp){
-						console.info(resp);
-					});
 				});
 			},
 			purchase: function (transactionId, productId, receipt){
